e058f440eaace4e507fbd2188ad86cffc0ac8b03
gau-rao <gauraonandekar99@gmail.com>
2021-07-07 04:11:55 +0530

    add GRAPHQL schema

---
+++ server/src/listings.ts
@@ -0,0 +1,43 @@
+interface Listing {
+    id: string,
+    title: string,
+    image : string,
+    address : string ,
+    price : number ,
+    numOfGuests  : number ,
+    numOfBeds : number,
+    rating : number
+}
+
+export const listings:Listing[] = [
+    {
+        id: "001" ,
+        title: "Clean and fully furnished 3-BHK apartment",
+        image : "https://img.staticmb.com/mbphoto/property/cropped_images/2020/Nov/30/Photo_h300_w450/42153139_8_77f74a8-5dba-41fc-a9ab-a5a642f4eea8_300_450.jpg",
+        address : "beltola guwahati",
+        price : 80000,
+        numOfGuests : 2,
+        numOfBeds : 2,
+        rating : 5
+    },
+    {
+        id : "002" ,
+        title: "luxurious home with private pool",
+        image : " https://img.staticmb.com/mbimages/project/Photo_h310_w462/Project-Photo-12-Ideal-Hill-View-Guwahati-5031131_561_1156_310_462.jpg",
+        address : "ideal hill view guwahati",
+        price : 50000,
+        numOfGuests : 1,
+        numOfBeds : 2 ,
+        rating : 4
+    },
+    {
+        id : "003" ,
+        title: "3 BHK apartment in birubari guwahati",
+        image : "https://img.staticmb.com/mbphoto/property/cropped_images/2021/May/06/Photo_h180_w240/55039455_2_lobalAshiyanaOption3-page-007_180_240.jpg",
+        address : "near bholenath temple guwahati",
+        price : 80000,
+        numOfGuests : 3,
+        numOfBeds : 3,
+        rating : 5
+    }
+]
