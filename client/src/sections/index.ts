585b2733102b3dbbdf0f4e8633b648140610f917
gau-rao <55942948+gau-rao@users.noreply.github.com>
2021-07-29 21:06:03 +0530

    exported all modules in sections

--- client/src/sections/index.ts
+++ client/src/sections/index.ts
@@ -2,5 +2,6 @@ export * from "./Listing";
 export * from "./Listings";
 export * from "./Host"; 
 export * from "./Home";
+export * from "./NotFound";
 export * from "./User";
 export * from "./NotFound";
