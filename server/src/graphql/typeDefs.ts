3ef1f4512d2f34c1704fd6f323b70e85612da274
gau-rao <gauraonandekar99@gmail.com>
2024-12-28 23:42:34 +0530

    Resolved merge conflict in <file>

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-import { gql } from "apollo-server-express";
-export const typeDefs = gql`
+import { gql } from "apollo-server-express";export const typeDefs = gql`
 type Listing {
     id : ID!
     title : String!
@@ -18,26 +16,4 @@ type Listing {
     type Mutation {
         deleteListing(id : ID!) : Listing ! 
     }
-    `; 
-=======
-import { gql } from "apollo-server-express";
-export const typeDefs = gql`
-type Listing {
-    id : ID!
-    title : String!
-    image : String!
-    address : String!
-    price : Int!
-    numOfGuests : Int!
-    numOfBeds : Int!
-    rating : Int!
-
-    }
-    type Query {
-        listings : [Listing!]!
-    }
-    type Mutation {
-        deleteListing(id : ID!) : Listing ! 
-    }
-    `; 
->>>>>>> e12acb5ea16ad9ec50cfdea64f8d240d3bc383b0
+    `;
